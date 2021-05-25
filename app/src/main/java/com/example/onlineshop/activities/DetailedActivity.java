7071ee7f11c526ecb4d16822b226c5a84243c2f6
dimitarnaumoski <dinaumoski@gmail.com>
2021-05-25 03:41:11 +0200

    SeeAllActivity,CategoryAdapter,DetailedActivity(+QuantityOption), CartActivity(Firestore Cloud Function),Added Toolbar in MainActivity

--- app/src/main/java/com/example/onlineshop/activities/DetailedActivity.java
+++ app/src/main/java/com/example/onlineshop/activities/DetailedActivity.java
@@ -1,33 +1,54 @@
 package com.example.onlineshop.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.onlineshop.R;
 import com.example.onlineshop.models.NewProductsModel;
 import com.example.onlineshop.models.PopularProductsModel;
+import com.example.onlineshop.models.ShowAllModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
 
 public class DetailedActivity extends AppCompatActivity {
 
     ImageView detailedImg;
-    TextView rating,name,description,price;
+    TextView rating,name,description,price,quantity;
     Button addToCart,butNow;
     ImageView addItems,removeItems;
 
+    int totalQuantity = 1;
+    int totalPrice = 0;
+
     // New Products
     NewProductsModel newProductsModel = null;
-    private FirebaseFirestore firestore;
+
 
     // Popular Products
     PopularProductsModel popularProductsModel = null;
 
+    // Show All
 
+    ShowAllModel showAllModel = null;
+
+    FirebaseAuth auth;
+    private FirebaseFirestore firestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +56,7 @@ public class DetailedActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detailed);
 
         firestore = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
 
         final Object obj = getIntent().getSerializableExtra("detailed");
 
@@ -42,9 +64,12 @@ public class DetailedActivity extends AppCompatActivity {
             newProductsModel = (NewProductsModel) obj;
         } else if (obj instanceof PopularProductsModel) {
             popularProductsModel = (PopularProductsModel) obj;
+        } else if (obj instanceof ShowAllModel) {
+            showAllModel = (ShowAllModel) obj;
         }
 
         detailedImg = findViewById(R.id.detailed_img);
+        quantity = findViewById(R.id.quantity);
         name = findViewById(R.id.detailed_name);
         rating = findViewById(R.id.rating);
         description = findViewById(R.id.detailed_desc);
@@ -64,6 +89,8 @@ public class DetailedActivity extends AppCompatActivity {
             description.setText(newProductsModel.getDescription());
             price.setText(String.valueOf(newProductsModel.getPrice()));
             name.setText(newProductsModel.getName());
+
+            totalPrice = newProductsModel.getPrice() * totalQuantity;
         }
 
         // Popular Products
@@ -74,8 +101,93 @@ public class DetailedActivity extends AppCompatActivity {
             description.setText(popularProductsModel.getDescription());
             price.setText(String.valueOf(popularProductsModel.getPrice()));
             name.setText(popularProductsModel.getName());
+
+            totalPrice = popularProductsModel.getPrice() * totalQuantity;
         }
 
+        // ShowAll Products
+        if (showAllModel != null) {
+            Glide.with(getApplicationContext()).load(showAllModel.getImg_url()).into(detailedImg);
+            name.setText(showAllModel.getName());
+            rating.setText(showAllModel.getRating());
+            description.setText(showAllModel.getDescription());
+            price.setText(String.valueOf(showAllModel.getPrice()));
+            name.setText(showAllModel.getName());
+
+            totalPrice = showAllModel.getPrice() * totalQuantity;
+        }
+
+        addToCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addToCart();
+            }
+        });
+
+        addItems.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (totalQuantity<10) {
+                    totalQuantity++;
+                    quantity.setText(String.valueOf(totalQuantity));
+
+                    if(newProductsModel != null) {
+                        totalPrice = newProductsModel.getPrice() * totalQuantity;
+                    }
+                    if (popularProductsModel != null) {
+                        totalPrice = popularProductsModel.getPrice() * totalQuantity;
+                    }
+                    if (showAllModel != null) {
+                        totalPrice = showAllModel.getPrice() * totalQuantity;
+                    }
+                }
+            }
+        });
+
+        removeItems.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (totalQuantity > 1) {
+                    totalQuantity--;
+                    quantity.setText(String.valueOf(totalQuantity));
+                }
+            }
+        });
 
     }
+            private void addToCart() {
+                String saveCurrentTime,saveCurrentDate;
+
+                Calendar calForDate = Calendar.getInstance();
+
+                SimpleDateFormat currentDate = new SimpleDateFormat("MMM dd, yyyy");
+                saveCurrentDate = currentDate.format(calForDate.getTime());
+
+                SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm:ss a");
+                saveCurrentTime = currentTime.format(calForDate.getTime());
+
+                final HashMap<String, Object> cartMap = new HashMap<>();
+
+                cartMap.put("productName", name.getText().toString());
+                cartMap.put("productPrice",price.getText().toString());
+                cartMap.put("currentTime",saveCurrentTime);
+                cartMap.put("currentDate",saveCurrentDate);
+                cartMap.put("totalQuantity",quantity.getText().toString());
+                cartMap.put("totalPrice", totalPrice);
+
+
+
+
+
+                firestore.collection("AddToCart").document(auth.getCurrentUser().getUid())
+                        .collection("User").add(cartMap).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentReference> task) {
+                        Toast.makeText(DetailedActivity.this, "Added To Cart", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                });
+
+            }
+
 }
--- app/src/main/java/com/example/onlineshop/fragments/HomeFragment.java
+++ app/src/main/java/com/example/onlineshop/fragments/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.example.onlineshop.fragments;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -13,12 +14,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
 import com.denzcoskun.imageslider.models.SlideModel;
 import com.example.onlineshop.R;
+import com.example.onlineshop.activities.ShowAllActivity;
 import com.example.onlineshop.adapters.CategoryAdapter;
 import com.example.onlineshop.adapters.NewProductsAdapter;
 import com.example.onlineshop.adapters.PopularProductsAdapter;
@@ -37,6 +40,7 @@ import java.util.List;
 
 public class HomeFragment extends Fragment {
 
+    TextView catShowAll,popularShowAll,newProductShowAll;
 
     int counter;
     RecyclerView catRecyclerview,newProductRecyclerview,popularRecyclerview;
@@ -67,12 +71,40 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
+        db = FirebaseFirestore.getInstance();
 
         catRecyclerview = root.findViewById(R.id.rec_category);
         newProductRecyclerview = root.findViewById(R.id.new_product_rec);
         popularRecyclerview = root.findViewById(R.id.popular_rec);
 
-        db = FirebaseFirestore.getInstance();
+        catShowAll = root.findViewById(R.id.category_see_all);
+        popularShowAll = root.findViewById(R.id.popular_see_all);
+        newProductShowAll = root.findViewById(R.id.newProducts_see_all);
+
+        catShowAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), ShowAllActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        newProductShowAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), ShowAllActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        popularShowAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), ShowAllActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
         //image slider
         ImageSlider imagesSlider = root.findViewById(R.id.image_slider);
